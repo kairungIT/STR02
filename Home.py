466abb0e3963e92862df0541fde7fbfa95bd7666
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-12-15 14:20:18 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.header("kairung hengpraprohm")
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
+st.subheader('สาขาวิชาวิทยาการข้อมูล')
+st.markdown("----")
