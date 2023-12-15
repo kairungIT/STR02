bc07b120f19f16e6c2ebcf7aa11adb9964ec60e4
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-12-15 14:27:09 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,3 +4,11 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
 st.subheader('สาขาวิชาวิทยาการข้อมูล')
 st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/K01.jpg')
+with col2:
+    st.image('./pic/iris.jpg')
