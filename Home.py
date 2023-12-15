955c45374f7c7df9b1a6113a4edce8bae421db56
kairungIT <113651426+kairungIT@users.noreply.github.com>
2023-12-15 15:49:25 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,20 +1,6 @@
 import streamlit as st
-import json
-from streamlit_lottie import st_lottie
-from streamlit_lottie import st_lottie_spinner
 
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
-lottie_url_download = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
-lottie_hello = load_lottieurl(lottie_url_hello)
-lottie_download = load_lottieurl(lottie_url_download)
-
-
+st.balloons()
 st.title('การทดสอบเขียนเว็บด้วย Python')
 st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
 st.subheader('สาขาวิชาวิทยาการข้อมูล')
